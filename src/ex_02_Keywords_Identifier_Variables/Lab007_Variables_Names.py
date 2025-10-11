4642e31ddcba9ea2499ab36b217f1e38086aa5f5
VishalDevkate <v.devkate@gmail.com>
2025-10-11 08:07:10 +0530

    new lab assignments

---
+++ src/ex_02_Keywords_Identifier_Variables/Lab007_Variables_Names.py
@@ -0,0 +1,9 @@
+_abc = 'Amit'
+isMale = True
+compl_num = 2 + 3j
+print((type(_abc)))
+print((type(isMale)))
+print((type(compl_num)))
+print("complex number: ",compl_num)
+print("real part: ", compl_num.real)
+print("imaginary part:", compl_num.imag)
